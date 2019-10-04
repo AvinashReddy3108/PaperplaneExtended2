a0c16eff4a783f52cbfff8ee1ee4920ebc754864
x0x8x <5467132+x0x8x@users.noreply.github.com>
2019-10-04 17:08:51 +0300

    adding function errors_handler
    Prevent an error ```ImportError: cannot import name 'errors_handler' from 'userbot.events' (/opt/PaperplaneExtended/userbot/events.py)``` causing by:
      File "/opt/PaperplaneExtended/userbot/modules/github.py", line 2, in <module>
        from userbot.events import register, errors_handler

--- userbot/events.py
+++ userbot/events.py
@@ -146,3 +146,62 @@ def register(**args):
         return wrapper
 
     return decorator
+
+   def errors_handler(func):
+    async def wrapper(errors):
+        try:
+            await func(errors)
+        except BaseException:
+            date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+            text = "**USERBOT CRASH REPORT**\n"
+            link = "[PaperplaneExtended Support Chat](https://t.me/PaperplaneExtendedSupport)"
+            text += "If you wanna you can report it"
+            text += f"- just forward this message to {link}.\n"
+            text += "Nothing is logged except the fact of error and date\n"
+
+            ftext = "========== DISCLAIMER =========="
+            ftext += "\nThis file uploaded ONLY here,"
+            ftext += "\nwe logged only fact of error and date,"
+            ftext += "\nwe respect your privacy,"
+            ftext += "\nyou may not report this error if you've"
+            ftext += "\nany confidential data here, no one will see your data\n"
+            ftext += "================================\n\n"
+            ftext += "--------BEGIN USERBOT TRACEBACK LOG--------"
+            ftext += "\nDate: " + date
+            ftext += "\nGroup ID: " + str(errors.chat_id)
+            ftext += "\nSender ID: " + str(errors.sender_id)
+            ftext += "\n\nEvent Trigger:\n"
+            ftext += str(errors.text)
+            ftext += "\n\nTraceback info:\n"
+            ftext += str(format_exc())
+            ftext += "\n\nError text:\n"
+            ftext += str(sys.exc_info()[1])
+            ftext += "\n\n--------END USERBOT TRACEBACK LOG--------"
+
+            command = "git log --pretty=format:\"%an: %s\" -5"
+
+            ftext += "\n\n\nLast 5 commits:\n"
+
+            process = await asyncsubshell(command,
+                                          stdout=asyncsub.PIPE,
+                                          stderr=asyncsub.PIPE)
+            stdout, stderr = await process.communicate()
+            result = str(stdout.decode().strip()) \
+            + str(stderr.decode().strip())
+
+            ftext += result
+
+            file = open("error.log", "w+")
+            file.write(ftext)
+            file.close()
+
+            await errors.client.send_file(
+                send_to,
+                "error.log",
+                caption=text,
+            )
+            remove("error.log")
+            return
+
+    return wrapper
